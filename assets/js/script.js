d98d5ea74448a4d46b44cdc85b39420dd0b3b0d7
SMakela <73409799+smakela13@users.noreply.github.com>
2021-02-24 15:52:35 -0500

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        question: "What company developed Javascript?",
+        choices: ["Apple", "Microsoft", "Sun Microsystems", "Netscape"],
+        answer: "Netscape"
+    }, {
+        question: "What HTML tag does JavaScript go inside?",
+        choices: ["<head>", "<script>", "<main>", "<footer>"],
+        answer: "<script>"
+    }, {
+        question: "What was JavaScript's original name?",
+        choices: ["LiveScript", "ECMAScript", "JavaScript", "Mocha"],
+        answer: "Mocha"
+    }, {
+        question: "What did Microsoft call their reverse-engineered version of JavaScript in 1996?",
+        choices: ["McScript", "iScript", "JScript", "Chakra"],
+        answer: "JScript"
+    }, {
+        question: "What is JavaScript best known for?",
+        choices: ["Static websites", "Basic styling", "Dynamic, interactive website experience", "Too many pop-up ads"],
+        answer: "Dynamic, interactive website experience"
+    }, {
+        question: "Is JavaScript similar to Java since they are named similarly?",
+        choices: ["Yes, JavaScript is a stripped down version of Java", "Yes, JavaScript was built by the same person who developed Java", "No, the names were a coincidence", "No, JavaScript gained its name when Netscape and Sun signed a license agreement"],
+        answer: "No, JavaScript gained its name when Netscape and Sun signed a license agreement"
+    }
+]
