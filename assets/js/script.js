dde1ef84572fc21ee607989f0aae769f24ff7786
SMakela <73409799+smakela13@users.noreply.github.com>
2021-02-28 22:21:56 -0500

    Refactored code and added comments to make cleaner and easier to view

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// ties the javascript with the HTML ids
 var highScoreEl = document.querySelector("#highscore");
 var quizTimerEl = document.querySelector("#quizTimer");
 var quizGameEl = document.querySelector("#jsQuiz");
@@ -5,28 +6,29 @@ var theQuestion = document.querySelector("#question");
 var theChoices = document.querySelector("#choices");
 var startButton = document.querySelector(".start-button");
 
-
+// global variables for quiz
 var submit;
+var indexQA = 0;
 var questions = 0;
 var currentQuestion = 0;
 var currentScore = 0;
 var endScore = 0;
-var gameDuration = 0;
+var isCorrect;
+var answered = false;
 var quizOver = false;
+var gameDuration = 0;
 var timer = 80;
 var timerN;
 var timerRun;
-var indexQA = 0;
-var cleaningList = [];
 var clearTimer = false;
-var isCorrect;
-var answered = false;
+var cleaningList = [];
 
-
+// function to initialize
 function init() {
     reset();
 }
 
+// function resets state of quiz
 function reset() {
     quizGameEl.innerHTML = "";
     currentScore = 0;
@@ -35,12 +37,14 @@ function reset() {
     answered = false;
 }
 
+// function clears elements from page after use
 function cleanList() {
     for (let i = 0; i < cleaningList.length; i++) {
         cleaningList[i].remove();
     }
 }
 
+// function contains questions and leads to quiz starting
 function startQuiz() {
     reset();
     questions = [
@@ -70,7 +74,6 @@ function startQuiz() {
             answer: "No, JavaScript gained its name when Netscape and Sun signed a license agreement"
         }
     ];
-
     quizOver = false;
     currentScore = 0;
     startButton.disabled = true;
@@ -84,6 +87,7 @@ function startQuiz() {
     startTimer();
 }
 
+// function begins timer and checks if timer should be running
 function startTimer() {
     timerN = setInterval(function () {
     if (gameDuration >= 0 && timerRun && !clearTimer) {
@@ -103,7 +107,64 @@ function startTimer() {
     }, 1000);
 }
 
-// Gets answer from user and matches if the answer is correct
+/* function renders the random question dynamically on the page 
+* and makes sure questions aren't repeated
+* also provides feedback to user if their answer is correct or wrong */
+function renderQuestion() {
+    indexQA = Math.floor(Math.random() * questions.length);
+    currentQuestion = questions[indexQA];
+    // questions appended to page
+    var questionN = document.createElement("h2");
+    questionN.textContent = currentQuestion.question;    
+    theQuestion.appendChild(questionN);
+    cleaningList.push(questionN);
+    // appended the choices, for loop due to choices being in array
+    for (let i = 0; i < currentQuestion.choices.length; i++) {
+        var choicesN = document.createElement("li");
+        choicesN.setAttribute("id", "choice" + i);
+        choicesN.textContent = currentQuestion.choices[i];
+        choicesN.addEventListener("click", choiceTaken);
+        theChoices.appendChild(choicesN);
+        cleaningList.push(choicesN);
+    }    
+    questions = arrayRemoveQuestion(questions, currentQuestion);
+    // correct answers
+    if (isCorrect && answered) {
+        var lineC = document.createElement("hr");
+        lineC.setAttribute("id", "line");
+        theChoices.appendChild(lineC);
+        cleaningList.push(lineC);
+
+        var correct = document.createElement("p");
+        correct.setAttribute("id", "correct");
+        correct.textContent = "Correct!";
+        theChoices.appendChild(correct);
+        cleaningList.push(correct);
+    }
+    // wrong answers
+    else if (!isCorrect && answered) {
+        var lineW = document.createElement("hr");
+        lineW.setAttribute("id", "line");
+        theChoices.appendChild(lineW);
+        cleaningList.push(lineW);
+
+        var wrong = document.createElement("p");
+        wrong.setAttribute("id", "choice");
+        wrong.textContent = "Wrong!";
+        theChoices.appendChild(wrong);
+        cleaningList.push(wrong);
+    }
+}
+
+// makes sure repeat questions are removed from array
+function arrayRemoveQuestion(array, child) {
+    return array.filter(function (elements) {
+        return elements != child;
+    });
+}
+
+/* function gets answer from user and checks if the answer is correct
+* if not, user is penalized by taking away 10 seconds */
 function answerCorrect(answerGiven) {
     isCorrect;
     answered = true;
@@ -117,69 +178,15 @@ function answerCorrect(answerGiven) {
     return isCorrect;
 }
 
-function renderQuestion() {
-    indexQA = Math.floor(Math.random() * questions.length);
-    currentQuestion = questions[indexQA];
-    // Made the question and appended it
-    var questionN = document.createElement("h2");
-    questionN.textContent = currentQuestion.question;    
-    theQuestion.appendChild(questionN);
-    cleaningList.push(questionN);
-
-    // Made and appended the choices - for loop due to choices being in array
-    for (let i = 0; i < currentQuestion.choices.length; i++) {
-        var choicesN = document.createElement("li");
-        choicesN.setAttribute("id", "choice" + i);
-        choicesN.textContent = currentQuestion.choices[i];
-        choicesN.addEventListener("click", choiceTaken);
-        theChoices.appendChild(choicesN);
-        cleaningList.push(choicesN);
-    }    
-    questions = arrayRemoveQuestion(questions, currentQuestion);
-    console.log("testset");
-    console.log(isCorrect);
-    console.log(answered);
-    if (isCorrect && answered) {
-        // correct
-        var line = document.createElement("hr");
-        line.setAttribute("id", "line");
-        theChoices.appendChild(line);
-        cleaningList.push(line);
-
-        var correct = document.createElement("p");
-        correct.setAttribute("id", "correct");
-        correct.textContent = "Correct!";
-        theChoices.appendChild(correct);
-        cleaningList.push(correct);
-
-    } else if (!isCorrect && answered) {
-        // wrong
-        var line = document.createElement("hr");
-        line.setAttribute("id", "line");
-        theChoices.appendChild(line);
-        cleaningList.push(line);
-
-        var wrong = document.createElement("p");
-        wrong.setAttribute("id", "choice");
-        wrong.textContent = "Wrong!";
-        theChoices.appendChild(wrong);
-        cleaningList.push(wrong);
-    }
-}
-
-function arrayRemoveQuestion(array, child) {
-    return array.filter(function (elements) {
-        return elements != child;
-    });
-}
-
+/* sees what choice the user picks and ends quiz 
+* depending on time or if there are questions 
+* remaining to be asked */
 function choiceTaken(elementFrom) {
-    var test = elementFrom.target;
+    var clickN = elementFrom.target;
     if (gameDuration <= 0) {
         endQuiz();
     } else {
-        answerCorrect(test.textContent);
-
+        answerCorrect(clickN.textContent); 
         askedQuestions();
         if (questions.length > 0) {
             renderQuestion();
@@ -189,34 +196,26 @@ function choiceTaken(elementFrom) {
     }
 }
 
+// clears asked questions from page after user answers
 function askedQuestions() {
     cleanList();
 }
 
-function getResults() {
-    var storedScores = JSON.parse(localStorage.getItem("leaderboard"));
-    return storedScores;
-}
-
-function clearResults() {
-    localStorage.setItem("leaderboard", null);
-    viewLeaderboard();
-}
-
+/* allows the user to view leaderboard and see current scores
+* tells user to take quiz if no scores are found */
 function viewLeaderboard() {
     cleanList();
     clearTimer = true;
     quizTimerEl.textContent = "";
+    startButton.style.display = "none";
 
     var storedResults = getResults();
     if (storedResults !== null) {
-        // add leaderboard header to jsQuiz ID
         var leaderboardHead = document.createElement("p");
         leaderboardHead.textContent = "Welcome to the leaderboard!";
         theQuestion.appendChild(leaderboardHead);
         cleaningList.push(leaderboardHead);
 
-        // added leaderboard to questions ID
         for (let i = 0; i < storedResults.length; i++) {
             var entryN = document.createElement("li");
             entryN.setAttribute("id", "name" + i);
@@ -224,16 +223,16 @@ function viewLeaderboard() {
             theQuestion.appendChild(entryN);
             cleaningList.push(entryN);
         }
-    } else {
-        // let user know no scores exist and ask them to play the game
+    }
+    // lets user know no scores exist and asks them to play the game
+    else {
         var viewResults = document.createElement("h3");
         viewResults.setAttribute("id", "view-results");
         viewResults.textContent = "No scores currently exist! Please take the quiz.";
         theQuestion.appendChild(viewResults);
         cleaningList.push(viewResults);
     }
-    
-    // need a clear high scores and play again button
+    // clear high scores and play again buttons
     var clearBoardBtn = document.createElement("button");
     clearBoardBtn.setAttribute("type", "button");
     clearBoardBtn.setAttribute("id", "clear-button");
@@ -251,9 +250,8 @@ function viewLeaderboard() {
     cleaningList.push(clearBoardBtn, playAgain);
 }
 
+// saves the user's initials and score to local storage
 function saveScore() {
-    console.log("save score here");
-
     var initials = document.querySelector("#initials").value;
     var storedResults = getResults();
     var leaderboardItem = {
@@ -265,14 +263,26 @@ function saveScore() {
     } else {
         storedResults = [leaderboardItem];
     }
-    
-    // in case of emergency - localStorage.setItem(initials, endScore);
     localStorage.setItem("leaderboard", JSON.stringify(storedResults));
-
     submit.style.display = "none";
     viewLeaderboard();
 }
 
+// gets results for leaderboard
+function getResults() {
+    var storedScores = JSON.parse(localStorage.getItem("leaderboard"));
+    return storedScores;
+}
+
+// clears the results on leaderboard
+function clearResults() {
+    localStorage.setItem("leaderboard", null);
+    viewLeaderboard();
+}
+
+/* end of the quiz where user finds out score and is asked
+* to put in their initials and if they want to play again
+* without entering their initials */
 function endQuiz() {
     console.log("EndGame");
 
@@ -312,6 +322,8 @@ function endQuiz() {
     cleaningList.push(submit, enterInitials, quizEnd, playAgain);
 }
 
+/* event listeners that allow user to start quiz or view leaderboard 
+* from initial load and init that calls to init function when page loads */
 highScoreEl.addEventListener("click", viewLeaderboard);
 startButton.addEventListener("click", startQuiz);
 init();
